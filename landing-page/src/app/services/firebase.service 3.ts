dc14befe2ce18a85e73b377990a834878cf89964
Adrian <moldovan.adrian.alexandru03@gmail.com>
2025-03-24 00:27:24 +0200

    Changed pictures plus other changes.

---
+++ landing-page/src/app/services/firebase.service 3.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { FirebaseApp, initializeApp } from 'firebase/app';
+import { environment } from '../../environments/environment';
+import { equalTo, get, getDatabase, orderByChild, push, ref, set } from 'firebase/database';
+import { query } from 'firebase/database';
+@Injectable({
+  providedIn: 'root'
+})
+
+export class FirebaseService {
+  private readonly app: FirebaseApp = initializeApp(environment);
+  private readonly db: any = getDatabase();
+  constructor() {
+    if(!this.app) {
+      throw new Error('Firebase app not initialized');
+    }
+  }
+
+   async addSubscriber(email: string): Promise<string> {
+    const reference = ref(this.db, 'subscribers/');
+    const emailQuery = query(reference, orderByChild('email'), equalTo(email));
+
+    try {
+      const snapshot = await get(emailQuery);
+
+      if(snapshot.exists()) {
+        console.warn('Email already subscribed', email);
+        return 'Email already subscribed';
+      }
+
+      const newSubscriberRef = push(reference);
+      await set(newSubscriberRef, { email });
+      console.log('Subscriber added successfully', email);
+      return 'Subscriber added successfully';
+    } catch(error){
+      console.error('Error checking for duplicates:', error);
+      return 'Error occurred';
+    }
+  }
+}
---
+++ landing-page/src/environments/environment 3.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  apiKey: "AIzaSyDJ6HoweFhuktVnfoJkE2UYFQ-uIRoTKp8",
+  authDomain: "arta-stradala-landing-page.firebaseapp.com",
+  databaseURL: "https://arta-stradala-landing-page-default-rtdb.firebaseio.com",
+  projectId: "arta-stradala-landing-page",
+  storageBucket: "arta-stradala-landing-page.firebasestorage.app",
+  messagingSenderId: "80985157169",
+  appId: "1:80985157169:web:2645045cbfedd42e99ef10"
+};
